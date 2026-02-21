4973d3f8f51f747c4f24202deea61c1ab9432768
ScriptB <asuneteric@gmail.com>
2026-02-21 01:57:01 +0000

    v3.5 - Notifications flush to bottom-right screen corner (zero offset)

--- Examples/Universal_ESP_Pro_Enhanced.lua
+++ Examples/Universal_ESP_Pro_Enhanced.lua
@@ -609,9 +609,9 @@ task.delay(10, function()
     end
 end)
 
--- NotificationArea: bottom-right, grows upward
+-- NotificationArea: bottom-right corner flush with screen edge
 Library.NotificationArea.AnchorPoint = Vector2.new(1, 1)
-Library.NotificationArea.Position    = UDim2.new(1, -10, 1, -10)
+Library.NotificationArea.Position    = UDim2.new(1, 0, 1, 0)
 Library.NotificationArea.Size        = UDim2.new(0, 300, 0, 600)
 -- Make the list layout stack from the bottom upward
 local _notifLayout = Library.NotificationArea:FindFirstChildOfClass("UIListLayout")
